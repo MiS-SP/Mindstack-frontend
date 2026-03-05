bf847822d8d5a0dfb0cda2f933ef3f532419f4f3
MiS-SP <misstartp@gmail.com>
2026-03-05 10:18:01 +0400

    improve UI

--- src/app/sections/resume/contact/contact.ts
+++ src/app/sections/resume/contact/contact.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-resume-contact',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
